fa336b1dcc3b700be2269a5989780b7861a5fcd3
Ngedenidze <ngedenidze@outlook.com>
2022-02-27 01:37:27 -0500

    Login page as starter window

--- demo/src/main/java/com/example/demo/App.java
+++ demo/src/main/java/com/example/demo/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("login-page.fxml"));
         scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Employee Management");
         stage.sizeToScene();
